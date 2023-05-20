2cbf56d6a08ef1aad3fb6533daf79c49bd6e6a47
Koray <74928882+koraydincc@users.noreply.github.com>
2023-05-20 18:27:17 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 const newData = [];
 let temp = '';
+let eleman = ''
 let counter = 0;
 let partiler = document.getElementById('partiler');
 let ad = document.getElementById('ad');
 let soyad = document.getElementById('soyad');
 let tc = document.getElementById('tc');
 let veri = document.getElementById('veri');
+let removeBtn = document.getElementById('removeBtn')
+let elemanSayisi = document.getElementById('elemanSayisi')
+let anketSonuclari = document.getElementById('anketSonuclari')
+
+let showSettingsContainerItem = document.getElementById('settingsContainer')
+let anketSonucBtn = document.getElementById('sonuc');
+
+// Değişen elemanın indeks değerini tutar
+let currentIndex = null;
+
+let anketChart = false;
+
 
 document.getElementById('kaydet').addEventListener('click', function() {
     let secilenParti = partiler.value;
+    let elemanSayisi = 0
+    
     
 
 
@@ -33,16 +48,130 @@ document.getElementById('kaydet').addEventListener('click', function() {
         return false;
     }
 
-    newData.push(ad.value);
-    newData.push(soyad.value);
-    newData.push(tc.value);
-    newData.push(secilenParti)
     
-    temp += `<li>Sayın ${tc.value} kimlik numaralı ${ad.value.toUpperCase()} ${soyad.value.toUpperCase()}. Oyunuzu ${secilenParti}'den yana kullandınız.</li>`;
-    veri.innerHTML = temp;
+   newData.push({
+    ad: ad.value,
+    soyad: soyad.value,
+    tc: tc.value,
+    secilenParti: secilenParti
+   })
+
+
+    counter++;
+
+ 
+    //   temp += ` <li>${counter}.Kayıt : Sayın ${tc.value} kimlik numaralı ${ad.value.toUpperCase()} ${soyad.value.toUpperCase()}. Oyunuzu ${secilenParti}'den yana kullandınız.</li>
+    //  <button type="button"
+    //   onclick="removeBtn2()" id="removeBtn";>Sil</button>` ;
+
+    elemanSayisi++;
+
+    elemanSayisi.innerHTML = `Toplam Üye = ${newData.length}`
+
+
+   readList();
+    
+
     ad.value = '';
     soyad.value = '';
     tc.value = '';
-
-    console.log(newData);
+    
+    
 });
+
+
+//chart.js  
+function readList() {
+
+
+  let temp = '';
+  for (let i = 0; i < newData.length; i++) {
+    temp += ` <li>${i + 1}.Kayıt : Sayın ${newData[i].tc} kimlik numaralı 
+          ${newData[i].ad.toUpperCase()} ${newData[i].soyad.toUpperCase()}. Oyunuzu ${newData[i].secilenParti}'den yana kullandınız.</li>
+     <button type="button"
+      onclick="removeBtn2(${i})" id="removeBtn";>Sil</button>
+          <button type="button"
+      onclick="showSettingsContainer(${i})" id="removeBtn";>Düzenle</button>
+      ` ;
+  } 
+
+
+  veri.innerHTML = temp;
+
+}
+
+  function removeBtn2(index) {
+    newData.splice(index,1)
+    readList();
+  }
+
+
+
+function showSettingsContainer(index) {
+  showSettingsContainerItem.style.display = 'block'
+  currentIndex = index;
+}
+
+
+function newDataUpdate() {
+  let updatePartiValue = document.getElementById('u_partiler').value;
+  newData[currentIndex].secilenParti = updatePartiValue
+  readList();
+
+}
+
+
+
+function anketSonuclariShow() {
+  anketSonuclari.style.display = 'block';
+
+  let anket = {
+    CHP: 0,
+    AKP: 0,
+    MHP: 0,
+    HDP: 0,
+    İYİ_PARTİ:0
+  };
+  
+  const ctx = document.getElementById('myChart');
+
+  for (let i = 0; i < newData.length; i++) {
+        anket[newData[i].secilenParti] = anket[newData[i].secilenParti] + 1 
+  }
+
+  if (typeof anketChart == 'object') {
+    anketChart.destroy(); 
+  }
+
+  anketChart =   new Chart(ctx, {
+    type: 'bar',
+    data: {
+    
+      labels: ['CHP', 'AKP', 'İYİ PARTİ', 'HDP', 'MHP'],
+      datasets: [{
+        label: 'Parti Oyları',
+        data: [anket.CHP, anket.AKP, anket.İYİ_PARTİ, anket.HDP, anket.MHP],
+        borderWidth: 1,
+            backgroundColor: [
+          'red',
+          'blue',
+          'blue',
+          'blue',
+          'blue'
+    ],
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+}
+
+
---
+++ object.js
@@ -0,0 +1,81 @@
+// let secimler = 'Ad Sezer, Soyad Boluk'
+
+
+let kisi = {
+    ad: 'Sezer' ,
+    soyad: 'Bölük',
+    yas: 27,
+    adres: 'lorem ipsum..',
+    'Ad Soyad': 'Sezer Bölük',
+    kisiBilgileri: function () {
+     // console.log(this.ad, this.soyad)
+     // console.log(kisi.ad, kisi.soyad)
+    }
+
+}
+
+// console.log(kisi['Ad Soyad'])
+
+// This
+
+
+// kisi.kisiBilgileri()
+
+
+
+
+// let car = {
+//     name: 'Fiyat',
+//     model: 'BWM'
+// }
+
+
+// let kisiler = [
+//     {
+//         ad: 'Sezer',
+//         soyad: 'Bölük'
+//     },
+//     {
+//         ad: 'Mehmet',
+//         soyad : 'x'
+//     }
+// ]
+
+// console.log(kisiler[1].ad)
+
+let persons = [
+    {
+        ad: 'Sezer',
+        soyad: 'Bölük'
+    },
+    {
+        ad: 'Mehmet',
+        soyad : 'x'
+    },
+
+    {
+        ad: 'Mehmet2',
+        soyad : 'x'
+    },
+          {
+        ad: 'Mehmet3',
+        soyad : 'x'
+    },
+    {
+           ad: 'Mehmet3',
+        soyad : 'x'       
+   }
+]
+
+
+// persons.splice(2,1)
+
+
+// persons[2].ad = 'Sezer';
+
+// console.log(persons)
+
+
+// for (let i = 0; i < persons.length; i++) {
+//    console.log( persons[i].ad, persons[i].soyad)
+// }
